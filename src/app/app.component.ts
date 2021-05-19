b5383b6f5ad785189114d3d258f5926d4f4fd14f
vinox59 <info.vinox@gmail.com>
2021-05-19 17:21:12 +0530

    Inital Commit
    Inital Commit for management app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'management';
+
+  ngOnInit() {
+    const userDetail = [{
+      id: 1,
+      username: 'user1',
+      password: 'user1'
+    }, {
+      id: 2,
+      username: 'user2',
+      password: 'user2'
+    }]
+    sessionStorage.setItem('userDetail',JSON.stringify(userDetail))
+  }
 }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {RouterModule, Router} from '@angular/router'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  userCredential: any = [];
+  isSuccess: boolean = false;
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.getUserDetailFromStorage();
+  }
+
+  getUserDetailFromStorage() {
+    let userInfo = sessionStorage.getItem('userDetail');
+    this.userCredential = JSON.parse(userInfo);
+
+  }
+
+  getUserDetails(val) {
+    let filterData = this.userCredential.find(credential => {
+      return (val.username === credential.username && val.password === credential.password) 
+    });
+    
+    if(filterData) {
+      this.router.navigate(['/home'])
+    } else {
+      this.isSuccess = true;
+    }
+  }
+
+}
